b303db75e2852a67dd28837eb2b728e14fc8eb52
SvelteRust <oddharald@myhren.ai>
2025-06-28 20:48:07 +0200

    need to add auth

--- lib/links_web.ex
+++ lib/links_web.ex
@@ -17,7 +17,7 @@ defmodule LinksWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images)
 
   def router do
     quote do
