1a618103927d50ac997e1820774dbabfd3b7c95a
SvelteRust <oddharald@myhren.ai>
2025-06-28 20:13:37 +0200

    sick, now fix ui

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.0.9",
+  version: "4.0.10",
   links: [
     args: ~w(
       --input=assets/css/app.css
